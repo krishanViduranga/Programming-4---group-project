5c1138fbb10ea7444b1447beddbdabcd9acee94d
Krishan Viduranga <kviduranga54@gmail.com>
2017-10-23 22:15:34 +0530

    Home Page Edit

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,45 +1,42 @@
 <header id="header"><!--header-->
-	<div class="header_top"><!--header_top-->
-			<div class="container">
+	<!-- <div class="header_top"><!--header_top-->
+			<!-- <div class="container">
 				<div class="row">
+					
 					<div class="col-sm-6">
-						<div class="contactinfo">
-							<ul class="nav nav-pills">
-								<li><a href=""><i class="fa fa-facebook"></i></a></li>
-								<li><a href=""><i class="fa fa-twitter"></i></a></li>
-								<li><a href=""><i class="fa fa-linkedin"></i></a></li>
-								<li><a href=""><i class="fa fa-dribbble"></i></a></li>
-								<li><a href=""><i class="fa fa-google-plus"></i></a></li>
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+								<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
 							</ul>
 						</div>
-					</div>					
+					</div>
 				</div>
-			</div>
-	</div><!--/header_top-->
+			</div> -->
+	<!-- </div --><!--/header_top--> 
 
 	<div class="header-middle"><!--header-middle-->
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-4">
 						<div class="logo pull-left">
-							<a href="{{ url('/home') }}"><img src="/images/home/logo.png" alt="" /></a>
-						</div>						
-					</div>
-					<div class="col-sm-8">						
-							<div class="shop-menu pull-right">
-								
-									<ul class="nav navbar-nav">
-																
-											<li><a href="#"><i class="fa fa-user"></i> Account</a></li>								
-											<li><a href="{{ url('/checkout') }}"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-											<li><a href="{{ url('/cart') }}"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-										
-											<li><a href="{{ url('/login') }}"><i class="fa fa-lock"></i> Login</a></li>
-
-									</ul>
-
-							</div>
+							<a href="index.html"><img src="/images/home/logo.png" alt="" /></a>
+						</div>
 						
+					</div>
+					<div class="col-sm-8">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-user"></i> Account</a></li>
+								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+								<li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+								<li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -49,7 +46,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-9">
-						<div class="navbar-header">
+						<!-- <div class="navbar-header">
 							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
@@ -64,20 +61,55 @@
                                     <ul role="menu" class="sub-menu">
                                         <li><a href="shop.html">Products</a></li>
 										<li><a href="product-details.html">Product Details</a></li> 
-										<li><a href="{{ url('/checkout') }}">Checkout</a></li> 
-										<li><a href="{{ url('/cart') }}">Cart</a></li> 
-										<li><a href="{{ url('/login') }}">Login</a></li> 
+										<li><a href="checkout.html">Checkout</a></li> 
+										<li><a href="cart.html">Cart</a></li> 
+										<li><a href="login.html">Login</a></li> 
                                     </ul>
-                                </li> 																
-								<li><a href="{{ url('contact') }}">Contact</a></li>
+                                </li> 
+								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
+                                    <ul role="menu" class="sub-menu">
+                                        <li><a href="blog.html">Blog List</a></li>
+										<li><a href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li> 
+								<li><a href="404.html">404</a></li>
+								<li><a href="contact-us.html">Contact</a></li>
 							</ul>
-						</div>
+						</div> -->
 					</div>
-					<div class="col-sm-3">
+					<!-- >>>>up content about the left side above part of the serchbar -->
+					<!-- <div class="col-sm-3">
 						<div class="search_box pull-right">
 							<input type="text" placeholder="Search"/>
 						</div>
+					</div> -->
+					<div class="container">
+					    <div class="row">    
+					        <div class="col-xs-8 col-xs-offset-2">
+							    <div class="input-group">
+					                <div class="input-group-btn search-panel">
+					                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+					                    	<span id="search_concept">Filter by</span> <span class="caret"></span>
+					                    </button>
+					                    <ul class="dropdown-menu" role="menu">
+					                      <li><a href="#contains">Contains</a></li>
+					                      <li><a href="#its_equal">It's equal</a></li>
+					                      <li><a href="#greather_than">Greather than ></a></li>
+					                      <li><a href="#less_than">Less than < </a></li>
+					                      <li class="divider"></li>
+					                      <li><a href="#all">Anything</a></li>
+					                    </ul>
+					                </div>
+					                <input type="hidden" name="search_param" value="all" id="search_param">         
+					                <input type="text" class="form-control" name="x" placeholder="Search term...">
+					                <span class="input-group-btn">
+					                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+					                </span>
+					            </div>
+					        </div>
+						</div>
 					</div>
+
 				</div>
 			</div>
 	</div><!--/header-bottom-->
